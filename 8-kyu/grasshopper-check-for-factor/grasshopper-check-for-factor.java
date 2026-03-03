f0d899147e2c00743ee0342d4b2f67a16e75914a
tnangsa <139562849+tnangsa@users.noreply.github.com>
2026-03-03 14:20:43 -0500

    Add solution - CodeHub

---
+++ 8-kyu/grasshopper-check-for-factor/grasshopper-check-for-factor.java
@@ -0,0 +1,10 @@
+public class Kata {
+    public static boolean checkForFactor(int base, int factor) {
+        // your code here
+      if(base % factor == 0){
+        return true;
+      }else{
+        return false;
+      }
+    }
+}
