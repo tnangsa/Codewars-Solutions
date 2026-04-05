2574c6ab27a3a5374e87728f0fc02202410b51a7
tnangsa <139562849+tnangsa@users.noreply.github.com>
2026-04-05 12:58:23 -0400

    Update solution - CodeHub

--- 8-kyu/abbreviate-a-two-word-name/abbreviate-a-two-word-name.py
+++ 8-kyu/abbreviate-a-two-word-name/abbreviate-a-two-word-name.py
@@ -1,7 +1,7 @@
 def abbrev_name(name):
-    name_parts = name.split()
+    name_parts = name.upper().split()
     
-    first_initial = name_parts[0][0].upper()
-    last_initial = name_parts[1][0].upper()
+    first_initial = name_parts[0][0]
+    last_initial = name_parts[1][0]
     
-    return f"{first_initial}.{last_initial}"
+    return f"{first_initial}.{last_initial}" 
