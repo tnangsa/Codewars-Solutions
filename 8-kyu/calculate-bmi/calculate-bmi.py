8cd4381077f68aa4f34c783034398640700bf6c4
tnangsa <139562849+tnangsa@users.noreply.github.com>
2026-04-08 10:35:41 -0400

    Add solution - CodeHub

---
+++ 8-kyu/calculate-bmi/calculate-bmi.py
@@ -0,0 +1,12 @@
+def bmi(weight, height):
+    #your code here
+    bmi = weight / (height * height)
+    
+    if bmi <= 18.5:
+        return "Underweight"
+    elif bmi <= 25.0:
+        return "Normal"
+    elif bmi <= 30.0:
+        return "Overweight"
+    elif bmi > 30:
+        return "Obese"
